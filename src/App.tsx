4dc449da19d340d8731f809b8cbc310c0ec2d76e
lilianwang <wanglijun00123172@gmail.com>
2023-01-31 09:51:36 -0500

    hero’s component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import { Carousel } from "./layouts/HomePage/Carousel";
 import { ExploreTopBooks } from "./layouts/HomePage/ExploreTopBooks";
+import { Heros } from "./layouts/HomePage/Heros";
 import { Navbar } from "./layouts/NavbarAndFooter/Navbar";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Navbar />
       <ExploreTopBooks />
       <Carousel />
+      <Heros />
     </div>
   );
 }
