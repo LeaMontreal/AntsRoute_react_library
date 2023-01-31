3e8d941f59c302930c5686715b4183313cf24945
lilianwang <wanglijun00123172@gmail.com>
2023-01-30 19:47:31 -0500

    explore top books component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import "./App.css";
+import { ExploreTopBooks } from "./layouts/HomePage/ExploreTopBooks";
 import { Navbar } from "./layouts/NavbarAndFooter/Navbar";
 
 function App() {
   return (
-  <Navbar />
+    <div>
+      <Navbar />
+      <ExploreTopBooks />
+    </div>
   );
 }
 
