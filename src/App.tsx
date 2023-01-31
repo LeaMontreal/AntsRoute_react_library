d27b7bf8bd5db251d849994638fc440bd78d6f4d
lilianwang <wanglijun00123172@gmail.com>
2023-01-31 10:05:24 -0500

    footer component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import { Carousel } from "./layouts/HomePage/Carousel";
 import { ExploreTopBooks } from "./layouts/HomePage/ExploreTopBooks";
+import { Footer } from "./layouts/NavbarAndFooter/Footer";
 import { Heros } from "./layouts/HomePage/Heros";
 import { LibraryServices } from "./layouts/HomePage/LibraryServices";
 import { Navbar } from "./layouts/NavbarAndFooter/Navbar";
@@ -13,7 +14,8 @@ function App() {
       <ExploreTopBooks />
       <Carousel />
       <Heros />
-      <LibraryServices/>
+      <LibraryServices />
+      <Footer />
     </div>
   );
 }
