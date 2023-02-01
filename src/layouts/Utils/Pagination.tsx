ebb3bff6c0864d2e2f8b59210124e175d8d43126
lilianwang <wanglijun00123172@gmail.com>
2023-02-01 17:03:28 -0500

    Pagination logic: generate page number list

--- src/layouts/Utils/Pagination.tsx
+++ src/layouts/Utils/Pagination.tsx
@@ -3,7 +3,34 @@ export const Pagination: React.FC<{
   totalPages: number;
   paginate: any;
 }> = (props) => {
-  return (<nav>
-    
-  </nav>);
+    // generate page number list
+  const pageNumbers = [];
+
+  if (props.currentPage === 1) {
+    pageNumbers.push(props.currentPage);
+    if (props.totalPages >= props.currentPage + 1) {
+      pageNumbers.push(props.currentPage + 1);
+    }
+    if (props.totalPages >= props.currentPage + 2) {
+      pageNumbers.push(props.currentPage + 2);
+    }
+  } else if (props.currentPage > 1) {
+    if (props.currentPage >= 3) {
+      pageNumbers.push(props.currentPage - 2);
+      pageNumbers.push(props.currentPage - 1);
+    } else {
+      pageNumbers.push(props.currentPage - 1);
+    }
+
+    pageNumbers.push(props.currentPage);
+
+    if (props.totalPages >= props.currentPage + 1) {
+      pageNumbers.push(props.currentPage + 1);
+    }
+    if (props.totalPages >= props.currentPage + 2) {
+      pageNumbers.push(props.currentPage + 2);
+    }
+  }
+
+  return <nav></nav>;
 };
