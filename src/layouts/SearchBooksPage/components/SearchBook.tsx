ee90c6bf3e1bc5d020491385e0251a2edae0f914
lilianwang <wanglijun00123172@gmail.com>
2023-02-01 14:24:41 -0500

    SearchBook component, step 2

--- src/layouts/SearchBooksPage/components/SearchBook.tsx
+++ src/layouts/SearchBooksPage/components/SearchBook.tsx
@@ -3,6 +3,24 @@ import BookModel from "../../../models/BookModel";
 export const SearchBook: React.FC<{book: BookModel}> = (props)=>{
 
     return (
-        
+        <div className='card mt-3 shadow p-3 mb-3 bg-body rounded'>
+            <div className='row g-0'>
+
+                {/* book image */}
+                <div className='col-md-2'>
+
+                </div>
+
+                {/* book description */}
+                <div className='col-md-6'>
+
+                </div>
+
+                {/* Detail button */}
+                <div className='col-md-4 d-flex justify-content-center align-items-center'>
+                    
+                </div>
+            </div>
+        </div>
     );
 }
