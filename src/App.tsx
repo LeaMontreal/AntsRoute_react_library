6e0ca2e3a5a0efb8437970b7b4a3738f248925d2
lilianwang <wanglijun00123172@gmail.com>
2023-02-02 12:01:09 -0500

    Keep Footer on Bottom of Page, add CSS

--- src/App.tsx
+++ src/App.tsx
@@ -8,21 +8,23 @@ import { Redirect, Route, Switch } from "react-router-dom";
 
 export const App = () => {
   return (
-    <div>
+    <div className="d-flex flex-column min-vh-100">
       <Navbar />
 
-      <Switch>
-        <Route path="/" exact>
-          <Redirect to="/home" />
-        </Route>
-        <Route path="/home">
-          <HomePage />
-        </Route>
-        <Route path="/search">
-          <SearchBooksPage />
-        </Route>
-      </Switch>
-
+      <div className="flex-grow-1">
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="/home" />
+          </Route>
+          <Route path="/home">
+            <HomePage />
+          </Route>
+          <Route path="/search">
+            <SearchBooksPage />
+          </Route>
+        </Switch>
+      </div>
+      
       <Footer />
     </div>
   );
