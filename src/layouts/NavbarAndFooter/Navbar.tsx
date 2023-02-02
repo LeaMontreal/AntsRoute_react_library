20f5f0d916c2a3e71917fb1780148744e775f519
lilianwang <wanglijun00123172@gmail.com>
2023-02-02 11:57:46 -0500

    Link Navbar nav-item, button link, footer nav-link

--- src/layouts/NavbarAndFooter/Navbar.tsx
+++ src/layouts/NavbarAndFooter/Navbar.tsx
@@ -1,3 +1,5 @@
+import { NavLink } from "react-router-dom";
+
 export const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark main-color py-3">
@@ -18,14 +20,12 @@ export const Navbar = () => {
         <div className="collapse navbar-collapse" id="navbarNavDropdown">
           <ul className="navbar-nav">
             <li className="nav-item">
-              <a className="nav-link" href="#">
+              <NavLink className="nav-link" to="/home">
                 Home
-              </a>
+              </NavLink>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">
-                Search Books
-              </a>
+            <NavLink className='nav-link' to='/search'>Search Books</NavLink>
             </li>
           </ul>
           <ul className="navbar-nav ms-auto">
