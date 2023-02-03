453f6d90dfe3affc234bfd3a0c529b4b624aaa8a
lilianwang <wanglijun00123172@gmail.com>
2023-02-02 19:21:29 -0500

    add useEffect() to send REST request

--- src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
+++ src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import BookModel from "../../models/BookModel";
+import { SpinnerLoading } from "../Utils/SpinnerLoading";
 
 export const BookCheckoutPage = () => {
   const [book, setBook] = useState<BookModel>();
@@ -7,11 +8,58 @@ export const BookCheckoutPage = () => {
   const [httpError, setHttpError] = useState(null);
 
   // const pathInfo = window.location.pathname;
-  const bookId = (window.location.pathname).split('/')[2];
+  const bookId = window.location.pathname.split("/")[2];
 
   useEffect(() => {
     // console.log(`pathInfo: ` + pathInfo);
   }, []);
+
+  useEffect(() => {
+    const fetchBooks = async () => {
+      const url: string = `http://localhost:8080/api/v1/books/${bookId}`;
+
+      const response = await fetch(url);
+
+      // guard clause
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
+
+      const responseJson = await response.json();
+
+      const loadedBook: BookModel = {
+          id: responseJson.id,
+          title: responseJson.title,
+          author: responseJson.author,
+          description: responseJson.description,
+          copies: responseJson.copies,
+          copiesAvailable: responseJson.copiesAvailable,
+          category: responseJson.category,
+          img: responseJson.img,
+        };
+
+      setBook(loadedBook);
+      setIsLoading(false);
+    };
+
+    // catch exception
+    fetchBooks().catch((error: any) => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
+  }, []);
+
+  if (isLoading) {
+    return <SpinnerLoading />;
+  }
+
+  if (httpError) {
+    return (
+      <div className="container m-5">
+        <p>{httpError}</p>
+      </div>
+    );
+  }
 
   return (
     <div className="container">
