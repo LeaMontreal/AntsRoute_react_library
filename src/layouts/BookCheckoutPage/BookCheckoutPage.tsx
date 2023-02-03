ed63f6c7f29e3655a586527fb77a6ab6e2ef78a2
lilianwang <wanglijun00123172@gmail.com>
2023-02-02 19:02:44 -0500

    get parameter bookId from path

--- src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
+++ src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
@@ -1,7 +1,22 @@
+import { useEffect, useState } from "react";
+import BookModel from "../../models/BookModel";
+
 export const BookCheckoutPage = () => {
+  const [book, setBook] = useState<BookModel>();
+  const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState(null);
+
+  // const pathInfo = window.location.pathname;
+  const bookId = (window.location.pathname).split('/')[2];
+
+  useEffect(() => {
+    // console.log(`pathInfo: ` + pathInfo);
+  }, []);
+
   return (
-    <div>
+    <div className="container">
       <h1>Empty Component</h1>
+      {/* <h1>${pathInfo}</h1> */}
     </div>
   );
 };
