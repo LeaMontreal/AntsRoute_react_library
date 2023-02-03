53efd419dfa0bf8d29d7300a68e2a1c8193cb236
lilianwang <wanglijun00123172@gmail.com>
2023-02-03 09:05:56 -0500

    Add CheckoutAndReviewBox to BookCheckoutPage

--- src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
+++ src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import BookModel from "../../models/BookModel";
 import { SpinnerLoading } from "../Utils/SpinnerLoading";
 import { StarsReview } from "../Utils/StarsReview";
+import { CheckoutAndReviewBox } from "./CheckoutAndReviewBox";
 
 export const BookCheckoutPage = () => {
   const [book, setBook] = useState<BookModel>();
@@ -87,6 +88,7 @@ export const BookCheckoutPage = () => {
               <StarsReview rating={3.5} size={32} />
             </div>
           </div>
+          <CheckoutAndReviewBox book={book} mobile={false} />
         </div>
       </div>
 
@@ -112,6 +114,7 @@ export const BookCheckoutPage = () => {
             <StarsReview rating={4.5} size={32} />
           </div>
         </div>
+        <CheckoutAndReviewBox book={book} mobile={true} />
       </div>
     </div>
   );
