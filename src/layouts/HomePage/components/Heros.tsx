7be656351f98e972a5728dd41114fcf5e1e3587a
lilianwang <wanglijun00123172@gmail.com>
2023-02-06 17:58:12 -0500

    branch merge

--- src/layouts/HomePage/components/Heros.tsx
+++ src/layouts/HomePage/components/Heros.tsx
@@ -43,8 +43,6 @@ export const Heros = () => {
         </div>
         </div>
         
-        </div>
-        
         {/* Mobile Heros */}
         <div className="d-lg-none">
           <div className="container">
