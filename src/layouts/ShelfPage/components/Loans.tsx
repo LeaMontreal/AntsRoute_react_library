ca9cff3b75f151592388c4ce26509a2105445dd5
lilianwang <wanglijun00123172@gmail.com>
2023-02-13 14:52:07 -0500

    DOM for Mobile view

--- src/layouts/ShelfPage/components/Loans.tsx
+++ src/layouts/ShelfPage/components/Loans.tsx
@@ -149,7 +149,93 @@ export const Loans = () => {
       </div>
 
       {/* Mobile */}
-      <div></div>
+      <div className="container d-lg-none mt-2">
+        {shelfCurrentLoans.length > 0 ? (
+          // List of loans
+          <>
+            <h5 className="mb-3">Current Loans: </h5>
+            {shelfCurrentLoans.map((shelfCurrentLoan) => (
+              <div key={shelfCurrentLoan.book.id}>
+                {/* Image of Book */}
+                <div className="d-flex justify-content-center align-items-center">
+                  {shelfCurrentLoan.book?.img ? (
+                    <img
+                      src={shelfCurrentLoan.book?.img}
+                      width="226"
+                      height="349"
+                      alt="Book"
+                    />
+                  ) : (
+                    <img
+                      src={require("./../../../Images/BooksImages/book-luv2code-1000.png")}
+                      width="226"
+                      height="349"
+                      alt="Book"
+                    />
+                  )}
+                </div>
+                {/* Info card of Book */}
+                <div className="card d-flex mt-5 mb-3">
+                  <div className="card-body container">
+                    <div className="mt-3">
+                      <h4>Loan Options</h4>
+                      {/* Show loan info based on daysLeft */}
+                      {shelfCurrentLoan.daysLeft > 0 && (
+                        <p className="text-secondary">
+                          Due in {shelfCurrentLoan.daysLeft} days.
+                        </p>
+                      )}
+                      {shelfCurrentLoan.daysLeft === 0 && (
+                        <p className="text-success">Due Today.</p>
+                      )}
+                      {shelfCurrentLoan.daysLeft < 0 && (
+                        <p className="text-danger">
+                          Past due by {shelfCurrentLoan.daysLeft} days.
+                        </p>
+                      )}
+                      {/* Operation button List */}
+                      <div className="list-group mt-3">
+                        <button
+                          className="list-group-item list-group-item-action"
+                          aria-current="true"
+                          data-bs-toggle="modal"
+                          data-bs-target={`#mobilemodal${shelfCurrentLoan.book.id}`}
+                        >
+                          Manage Loan
+                        </button>
+                        <Link
+                          to={"search"}
+                          className="list-group-item list-group-item-action"
+                        >
+                          Search more books?
+                        </Link>
+                      </div>
+                    </div>
+                    <hr />
+                    <p className="mt-3">
+                      Help other find their adventure by reviewing your loan.
+                    </p>
+                    <Link
+                      className="btn btn-primary"
+                      to={`/checkout/${shelfCurrentLoan.book.id}`}
+                    >
+                      Leave a review
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </>
+        ) : (
+          // No loans
+          <>
+            <h3 className="mt-3">Currently no loans</h3>
+            <Link className="btn btn-primary" to={`search`}>
+              Search for a new book
+            </Link>
+          </>
+        )}
+      </div>
     </div>
   );
 };
