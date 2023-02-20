5aa99ee314d94d3d3beefeaadd957230d62778d0
lilianwang <wanglijun00123172@gmail.com>
2023-02-19 20:18:49 -0500

    Send new book request to BackEnd

--- src/layouts/ManageLibraryPage/components/AddNewBook.tsx
+++ src/layouts/ManageLibraryPage/components/AddNewBook.tsx
@@ -1,120 +1,216 @@
 import { useOktaAuth } from "@okta/okta-react"; // manually change
 import { useState } from "react";
+import AddBookRequest from "../../../models/AddBookRequest";
 
 export const AddNewBook = () => {
-    const { authState } = useOktaAuth();
+  const { authState } = useOktaAuth();
 
-    // New Book
-    const [title, setTitle] = useState('');
-    const [author, setAuthor] = useState('');
-    const [description, setDescription] = useState('');
-    const [copies, setCopies] = useState(0);
-    const [category, setCategory] = useState('Category');
-    const [selectedImage, setSelectedImage] = useState<any>(null);
+  // New Book
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const [description, setDescription] = useState("");
+  const [copies, setCopies] = useState(0);
+  const [category, setCategory] = useState("Category");
+  const [selectedImage, setSelectedImage] = useState<any>(null);
 
-    // Displays
-    const [displayWarning, setDisplayWarning] = useState(false);
-    const [displaySuccess, setDisplaySuccess] = useState(false);
+  // Displays
+  const [displayWarning, setDisplayWarning] = useState(false);
+  const [displaySuccess, setDisplaySuccess] = useState(false);
 
-    function categoryField(value: string) {
-        setCategory(value);
+  function categoryField(value: string) {
+    setCategory(value);
+  }
+
+  async function base64ConversionForImages(e: any) {
+    // console.log(e);
+
+    if (e.target.files[0]) {
+      getBase64(e.target.files[0]);
     }
+  }
 
-    async function base64ConversionForImages(e: any){
-        // console.log(e);
+  // read local file and save result into selectedImage useState()
+  function getBase64(file: any) {
+    let reader = new FileReader();
+    reader.readAsDataURL(file);
 
-        if(e.target.files[0]){
-            getBase64(e.target.files[0]);
-        }
+    // read file success
+    reader.onload = function () {
+      setSelectedImage(reader.result);
+    };
+    // read file error
+    reader.onerror = function (error) {
+      console.log("File Reader Error", error);
+    };
+  }
+
+  async function submitNewBook() {
+    const url = `http://localhost:8080/api/v1/admin/secure/add/book`;
+    if (authState?.isAuthenticated && title !== "" && author !== "" &&       category !== "Category" && description !== "" && copies >= 0) {
+      const book: AddBookRequest = new AddBookRequest(title, author,    description, copies, category);
+      book.img = selectedImage;
+      const requestOptions = {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${authState?.accessToken?.accessToken}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(book),
+      };
+
+      const submitNewBookResponse = await fetch(url, requestOptions);
+      if (!submitNewBookResponse.ok) {
+        throw new Error("Something went wrong!");
+      }
+
+      // sucess, clear input of the form
+      setTitle("");
+      setAuthor("");
+      setDescription("");
+      setCopies(0);
+      setCategory("Category");
+      setSelectedImage(null);
+
+      // display success prompt
+      setDisplayWarning(false);
+      setDisplaySuccess(true);
+    } else {
+      // display warning prompt
+      setDisplayWarning(true);
+      setDisplaySuccess(false);
     }
+  }
 
-    // read local file and save result into selectedImage useState()
-    function getBase64(file: any){
-        let reader = new FileReader();
-        reader.readAsDataURL(file);
-
-        // read file success
-        reader.onload = function(){
-            setSelectedImage(reader.result);
-        };
-        // read file error
-        reader.onerror = function(error){
-            console.log('File Reader Error', error)
-        }
-    }
-
-    return (
-        <div className='container mt-5 mb-5'>
-            {/* prompt message bar */}
-            {displaySuccess && 
-                <div className='alert alert-success' role='alert'>
-                    Book added successfully
-                </div>
-            }
-            {displayWarning && 
-                <div className='alert alert-danger' role='alert'>
-                    All fields must be filled out
-                </div>
-            }
-            {/* Add book card */}
-            <div className='card'>
-                <div className='card-header'>
-                    Add a new book
-                </div>
-                <div className='card-body'>
-                    <form method='POST'>
-                        {/* Title,Author,Category */}
-                        <div className='row'>
-                            {/* Title input */}
-                            <div className='col-md-6 mb-3'>
-                                <label className='form-label'>Title</label>
-                                <input type="text" className='form-control' name='title' required 
-                                    onChange={e => setTitle(e.target.value)} value={title} />
-                            </div>
-                            {/* Author input */}
-                            <div className='col-md-3 mb-3'>
-                                <label className='form-label'> Author </label>
-                                <input type="text" className='form-control' name='author' required 
-                                    onChange={e => setAuthor(e.target.value)} value={author}/>
-                            </div>
-                            {/* Category dropdown-menu */}
-                            <div className='col-md-3 mb-3'>
-                                <label className='form-label'> Category</label>
-                                <button className='form-control btn btn-secondary dropdown-toggle' type='button' 
-                                    id='dropdownMenuButton1' data-bs-toggle='dropdown' aria-expanded='false'>
-                                        {category}
-                                </button>
-                                <ul id='addNewBookId' className='dropdown-menu' aria-labelledby='dropdownMenuButton1'>
-                                    <li><a onClick={() => categoryField('FrontEnd')} className='dropdown-item'>Front End</a></li>
-                                    <li><a onClick={() => categoryField('BackEnd')} className='dropdown-item'>Back End</a></li>
-                                    <li><a onClick={() => categoryField('Data')} className='dropdown-item'>Data</a></li>
-                                    <li><a onClick={() => categoryField('DevOps')} className='dropdown-item'>DevOps</a></li>
-                                </ul>
-                            </div>
-                        </div>
-                        {/* Description */}
-                        <div className='col-md-12 mb-3'>
-                            <label className='form-label'>Description</label>
-                            <textarea className='form-control' id='exampleFormControlTextarea1' rows={3} 
-                                onChange={e => setDescription(e.target.value)} value={description}></textarea>
-                        </div>
-                        {/* Copies */}
-                        <div className='col-md-3 mb-3'>
-                            <label className='form-label'>Copies</label>
-                            <input type='number' className='form-control' name='Copies' required 
-                                onChange={e => setCopies(Number(e.target.value))} value={copies}/>
-                        </div>
-                        {/* File picker */}
-                        <input type='file' onChange={e=>base64ConversionForImages(e)}/>
-                        {/* Submit Button */}
-                        <div>
-                            <button type='button' className='btn btn-primary mt-3'>
-                                Add Book
-                            </button>
-                        </div>
-                    </form>
-                </div>
-            </div>
+  return (
+    <div className="container mt-5 mb-5">
+      {/* prompt message bar */}
+      {displaySuccess && (
+        <div className="alert alert-success" role="alert">
+          Book added successfully
         </div>
-    );
+      )}
+      {displayWarning && (
+        <div className="alert alert-danger" role="alert">
+          All fields must be filled out
+        </div>
+      )}
+      {/* Add book card */}
+      <div className="card">
+        <div className="card-header">Add a new book</div>
+        <div className="card-body">
+          <form method="POST">
+            {/* Title,Author,Category */}
+            <div className="row">
+              {/* Title input */}
+              <div className="col-md-6 mb-3">
+                <label className="form-label">Title</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  name="title"
+                  required
+                  onChange={(e) => setTitle(e.target.value)}
+                  value={title}
+                />
+              </div>
+              {/* Author input */}
+              <div className="col-md-3 mb-3">
+                <label className="form-label"> Author </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  name="author"
+                  required
+                  onChange={(e) => setAuthor(e.target.value)}
+                  value={author}
+                />
+              </div>
+              {/* Category dropdown-menu */}
+              <div className="col-md-3 mb-3">
+                <label className="form-label"> Category</label>
+                <button
+                  className="form-control btn btn-secondary dropdown-toggle"
+                  type="button"
+                  id="dropdownMenuButton1"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  {category}
+                </button>
+                <ul
+                  id="addNewBookId"
+                  className="dropdown-menu"
+                  aria-labelledby="dropdownMenuButton1"
+                >
+                  <li>
+                    <a
+                      onClick={() => categoryField("FrontEnd")}
+                      className="dropdown-item"
+                    >
+                      Front End
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      onClick={() => categoryField("BackEnd")}
+                      className="dropdown-item"
+                    >
+                      Back End
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      onClick={() => categoryField("Data")}
+                      className="dropdown-item"
+                    >
+                      Data
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      onClick={() => categoryField("DevOps")}
+                      className="dropdown-item"
+                    >
+                      DevOps
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            {/* Description */}
+            <div className="col-md-12 mb-3">
+              <label className="form-label">Description</label>
+              <textarea
+                className="form-control"
+                id="exampleFormControlTextarea1"
+                rows={3}
+                onChange={(e) => setDescription(e.target.value)}
+                value={description}
+              ></textarea>
+            </div>
+            {/* Copies */}
+            <div className="col-md-3 mb-3">
+              <label className="form-label">Copies</label>
+              <input
+                type="number"
+                className="form-control"
+                name="Copies"
+                required
+                onChange={(e) => setCopies(Number(e.target.value))}
+                value={copies}
+              />
+            </div>
+            {/* File picker */}
+            <input type="file" onChange={(e) => base64ConversionForImages(e)} />
+            {/* Submit Button */}
+            <div>
+              <button onClick={submitNewBook} type="button" className="btn btn-primary mt-3">
+                Add Book
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
