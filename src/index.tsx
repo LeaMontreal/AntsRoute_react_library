b421d88dab8836bab255a2ccb6b3daebf1cb0c01
lilianwang <wanglijun00123172@gmail.com>
2023-02-23 10:10:38 -0500

    Integrate Stripe

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,19 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { App } from "./App";
 import { BrowserRouter } from 'react-router-dom';
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+
+// loads the Stripe.js script and initializes a Stripe object
+const stripePromise = loadStripe('pk_test_51MUWRkGZOyLNVjLVIJCf5buo7nhKTYFTeK6mCMf1VMwTIjU2DN6EGCnmQYxTc2oJ5f44X6Y48CAzPdTLLmEPiCsF00s2teiP5g');
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <BrowserRouter>
+  <Elements stripe={stripePromise}>
     <App />
+  </Elements>
   </BrowserRouter>
 );
