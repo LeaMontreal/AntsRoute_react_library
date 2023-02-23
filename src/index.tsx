af5c8340d51bc73fe65b5d629fc5dae08f1b2bb5
lilianwang <wanglijun00123172@gmail.com>
2023-02-23 16:20:46 -0500

    Pay Fees Implement

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,8 @@ import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 
 // loads the Stripe.js script and initializes a Stripe object
+// Make sure to call `loadStripe` outside of a component’s render to avoid
+// recreating the `Stripe` object on every render.
 const stripePromise = loadStripe('pk_test_51MUWRkGZOyLNVjLVIJCf5buo7nhKTYFTeK6mCMf1VMwTIjU2DN6EGCnmQYxTc2oJ5f44X6Y48CAzPdTLLmEPiCsF00s2teiP5g');
 
 const root = ReactDOM.createRoot(
--- src/layouts/PaymentPage/PaymentPage.tsx
+++ src/layouts/PaymentPage/PaymentPage.tsx
@@ -1,6 +1,8 @@
 import { useOktaAuth } from "@okta/okta-react"; // mannually change
+import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import PaymentInfoRequest from "../../models/PaymentInfoRequest";
 import { SpinnerLoading } from "../Utils/SpinnerLoading";
 
 export const PaymentPage = () => {
@@ -9,6 +11,9 @@ export const PaymentPage = () => {
   const [fees, setFees] = useState(0);
   const [httpError, setHttpError] = useState(false);
   const [isLoadingFees, setIsLoadingFees] = useState(true);
+
+  // lock flag for avoiding repeat sending paymentInfo
+  const [submitDisabled, setSubmitDisabled] = useState(false);
 
   // myDebugForOkta
   const authorizationString = `Bearer `;
@@ -58,6 +63,75 @@ export const PaymentPage = () => {
     );
   }
 
+  const elements = useElements();
+  const stripe = useStripe();
+
+  async function checkout() {
+      if (!stripe || !elements || !elements.getElement(CardElement)) {
+          return;
+      }
+
+      // set lock flag for avoiding repeat sending paymentInfo
+      setSubmitDisabled(true);
+
+      let paymentInfo = new PaymentInfoRequest(Math.round(fees * 100), 'USD', userEmail);
+
+      const url = `${process.env.REACT_APP_BASE_URL}/payment/secure/payment-intent`;
+      const requestOptions = {
+          method: 'POST',
+          headers: {
+              Authorization: authorizationString,
+              'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(paymentInfo)
+      };
+      const stripeResponse = await fetch(url, requestOptions);
+      if (!stripeResponse.ok) {
+          setHttpError(true);
+          // open lock flag
+          setSubmitDisabled(false);
+          throw new Error('Something went wrong!');
+      }
+      const stripeResponseJson = await stripeResponse.json();
+
+      stripe.confirmCardPayment(
+          stripeResponseJson.client_secret, {
+              payment_method: {
+                  card: elements.getElement(CardElement)!,
+                  billing_details: {
+                      email: userEmail
+                  }
+              }
+          }, {handleActions: false}
+      ).then(async function (result: any) {
+          if (result.error) {
+              // open lock flag
+              setSubmitDisabled(false)
+              alert('There was an error')
+          } else {
+              const url = `${process.env.REACT_APP_BASE_URL}/payment/secure/payment-complete`;
+              const requestOptions = {
+                  method: 'PUT',
+                  headers: {
+                      Authorization: authorizationString,
+                      'Content-Type': 'application/json'
+                  }
+              };
+              const stripeResponse = await fetch(url, requestOptions);
+              if (!stripeResponse.ok) {
+                  setHttpError(true)
+                  // open lock flag
+                  setSubmitDisabled(false)
+                  throw new Error('Something went wrong!')
+              }
+              setFees(0);
+              // open lock flag
+              setSubmitDisabled(false);
+          }
+      });
+      setHttpError(false);
+  }
+
   return (
     <div className="container">
       {fees > 0 && (
@@ -67,8 +141,9 @@ export const PaymentPage = () => {
           </h5>
           <div className="card-body">
             <h5 className="card-title mb-3">Credit Card</h5>
-            <p>Credit Card Info</p>
-            <button
+            <CardElement id="card-element"/>
+            <button disabled={submitDisabled}
+              onClick={checkout}
               type="button"
               className="btn btn-md main-color text-white mt-3">
               Pay fees
